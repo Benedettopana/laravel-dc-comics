840f36e99dd652b2be10aae62d7800d830a63b5f
Benedettopana <benedetto.pana@gmail.com>
2024-05-16 18:05:36 +0200

    [add]form(post)/reindirizzamento alla nuova rotta

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,4 +4,5 @@
     <a href="{{route('home')}}">Home</a>
     <a href="{{route('nuova-pagina')}}">Nuova Pagina</a>
     <a href="{{route('comics.index')}}">Comics</a>
+    <a href="{{route('comics.create')}}">Nuovo Comic</a>
 </header>
